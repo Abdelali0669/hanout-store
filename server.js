a994773a79c75c67ea2f60ab67ad2f1b6f3fb5e3
Abdelali0669 <155241903+Abdelali0669@users.noreply.github.com>
2026-06-26 15:30:14 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// استخدام المسار الحالي مباشرة
-app.use(express.static('./'));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
