cf67810cf2d99a6623f58a6074a7e252be3a1850
Abdelali0669 <155241903+Abdelali0669@users.noreply.github.com>
2026-06-26 17:31:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.static(__dirname));
-
-// كل الصفحات المتاحة
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.get('/cart', (req, res) => res.sendFile(__dirname + '/cart.html'));
 app.get('/contact', (req, res) => res.sendFile(__dirname + '/contact.html'));
 app.get('/checkout', (req, res) => res.sendFile(__dirname + '/checkout.html'));
-
 app.listen(port, '0.0.0.0', () => console.log('المتجر يعمل!'));
